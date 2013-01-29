8edb68516b9d0d995963a3d584edb79a7e64960c
Shoadi Yokoyama <quantumcars@gmail.com>
2013-01-29 22:38:10 +0900

    Type: Add http request type

--- Network/Panpipes/HTTP/Type.hs
+++
@@ -1,8 +0,0 @@
-module Network.Panpipes.HTTP.Type where
-
-data Method = Option | Get | Head | Delete | Trace | Connect | Post | Put
-            deriving (Show, Eq)
-
-
-data Version = Version { major :: Int , minor :: Int }
-               deriving (Show, Eq)
---
+++ Network/Panpipes/HTTP/Types.hs
@@ -0,0 +1,18 @@
+module Network.Panpipes.HTTP.Types where
+
+import Data.ByteString
+
+data Method = Option | Get | Head | Delete | Trace | Connect | Post | Put
+            deriving (Show, Eq)
+
+
+data Version = Version { major :: Int , minor :: Int }
+               deriving (Show, Eq)
+
+
+data PartialRequest = PartialRequest
+    { method  :: Method
+    , uri     :: ByteString
+    , version :: Version
+    , headers :: [(ByteString, ByteString)]
+    } deriving (Show, Eq)
