aed86ee010332f0dd2423f051a5205b80f39e4fb
Shoadi Yokoyama <quantumcars@gmail.com>
2013-01-29 22:17:26 +0900

    Type: Add http version type

--- Network/Panpipes/HTTP/Type.hs
+++ Network/Panpipes/HTTP/Type.hs
@@ -2,3 +2,7 @@ module Network.Panpipes.HTTP.Type where
 
 data Method = Option | Get | Head | Delete | Trace | Connect | Post | Put
             deriving (Show, Eq)
+
+
+data Version = Version { major :: Int , minor :: Int }
+               deriving (Show, Eq)
