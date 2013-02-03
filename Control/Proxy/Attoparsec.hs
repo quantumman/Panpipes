ebee1035f9b14fdea9a953ae4ab02b61f8d30ba3
Shoadi Yokoyama <quantumcars@gmail.com>
2013-02-03 22:25:17 +0900

    Add parseD parsing on "proxy"

--- Control/Proxy/Attoparsec.hs
+++ Control/Proxy/Attoparsec.hs
@@ -1,6 +1,10 @@
 module Control.Proxy.Attoparsec where
 
+import Control.Proxy
+import Data.Attoparsec.ByteString hiding (Parser(..))
+import Data.Attoparsec.Types
 import qualified Data.ByteString as Strict
+import Data.ByteString.Char8 (pack)
 
 
 data ParsingResult r
@@ -9,3 +13,17 @@ data ParsingResult r
     { context :: [String]
     , message :: String
     }
+
+
+parseD :: (Monad m, Proxy p)
+       => Parser Strict.ByteString r
+       -> ()
+       -> Pipe p Strict.ByteString (ParsingResult r) m ()
+parseD parser _ = runIdentityP $ go (parse parser)
+  where
+    go k0 = do
+      xs <- request ()
+      case k0 xs of
+        Partial k -> go k
+        Fail _ context message -> respond $ Error context message
+        Done rest r -> respond $ Success r rest
