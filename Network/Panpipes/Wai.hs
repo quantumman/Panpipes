d5d1f41026a724e837011146af0388d77bd9c3cd
Shoadi Yokoyama <quantumcars@gmail.com>
2013-02-03 22:49:37 +0900

    Wai: Add http request type

--- Network/Panpipes/Wai.hs
+++ Network/Panpipes/Wai.hs
@@ -3,6 +3,16 @@ module Network.Panpipes.Wai where
 import Control.Proxy
 import Control.Proxy.Safe
 import Data.ByteString
+import qualified Network.Panpipes.HTTP.Types as HTTP
 
 
 type Body p = Producer p ByteString SafeIO ()
+
+
+data Request p = Request
+                 { method  :: HTTP.Method
+                 , version :: HTTP.Version
+                 , rowUri  :: ByteString
+                 , requestHeaders :: [(ByteString, ByteString)]
+                 , requestBody    :: Body p
+                 }
